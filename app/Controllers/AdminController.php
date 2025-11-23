89b51766345a7a2ef24b4a0de6482f26ae2b9127
Ozbert <202310393@fit.edu.ph>
2025-11-23 22:00:13 +0800

    feat: enhance user management view with dynamic user data and role badges

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -4,31 +4,25 @@ namespace App\Controllers;
 
 class AdminController extends BaseController
 {
-    public function users()
-    {
-        $data = [
-            'title' => 'User Management - ITSO EMS',
-            'bodyClass' => 'users-page',
-        ];
+   public function users()
+{
+    $usersModel = new \App\Models\Users_model();
 
-        return view('include/head_view', $data)
-            . view('include/nav_view', $data)      // render sidebar here
-            . view('users_view', $data)
-            . view('include/foot_view', $data);
-    }
-    public function equipment()
-    {
-        $data = [
-            'title' => 'Equipment Management - ITSO EMS',
-            'bodyClass' => 'equipment-page',
-            'active' => 'equipment'
-        ];
+    // Fetch all users from DB
+    $allUsers = $usersModel->findAll();
 
-        return view('include/head_view', $data)
-            . view('include/nav_view', $data)
-            . view('equipment_view', $data)   // front-end only
-            . view('include/foot_view', $data);
-    }
+    $data = [
+        'title' => 'User Management - ITSO EMS',
+        'bodyClass' => 'users-page',
+        'users' => $allUsers
+    ];
+
+    return view('include/head_view', $data)
+        . view('include/nav_view', $data)
+        . view('users_view', $data)
+        . view('include/foot_view', $data);
+}
+
 
 }
 
