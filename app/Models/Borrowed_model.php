57b6dbd7587ea09f038ec7ba6a54ccb1cc0f7dcd
Ozbert <202310393@fit.edu.ph>
2025-11-24 16:36:15 +0800

    add

---
+++ app/Models/Borrowed_model.php
@@ -0,0 +1,24 @@
+<?php 
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Borrowed_model extends Model
+{
+    protected $table = 'borrowed_items';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+
+    protected $returnType = 'array';
+
+    protected $allowedFields = [
+        'borrower_id',
+        'email',
+        'equipment_id',
+        'return_date'
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected $useTimestamps = false;
+}
+?>
